53ee3c414aa89cb6c1a786834cc414feee3c4480
soficruz2004 <149535446+soficruz2004@users.noreply.github.com>
2024-12-03 12:56:32 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "uiRenderer.h"
+using namespace std;
+
+
+
+int main() {
+    //create window
+    sf::RenderWindow welcomewindow(sf::VideoMode(800,700), "Travel Planner");
+
+    //load fonts
+    sf:: Font bold;
+    sf:: Font bolditalic;
+    sf:: Font regular;
+    if(!bold.loadFromFile("/Users/angelacruz/CLionProjects/travel_planner/resources/NotoSans-Bold.ttf")
+    || !bolditalic.loadFromFile("/Users/angelacruz/CLionProjects/travel_planner/resources/NotoSans-BoldItalic.ttf")
+    || !regular.loadFromFile("/Users/angelacruz/CLionProjects/travel_planner/resources/NotoSans-Regular.ttf")){
+        cerr<< "Error loading font" << endl;
+        return -1;
+    }
+
+    // make all welcome texts
+    sf::Text welcometext = createText(bold, "Welcome to ", 35, sf::Color::White, 176, 100);
+    sf::Text boldwelc = createText(bolditalic, "Travel Planner", 35, sf::Color::Black, 392, 100);
+    sf::Text exclamation = createText(bolditalic, "!", 35, sf::Color::White, 635, 100);
+    sf::Text whenvisit = createText(regular, "When are you planning your visit?", 28, sf::Color::White, 99, 170 );
+    sf::Text month = createText(regular, "Month: ", 23, sf::Color::White, 120, 230 );
+    sf::Text week = createText(regular, "Week: ", 23, sf::Color::White, 395, 230 );
+    sf::Text wherego = createText(regular, "Where would you like to go?", 28, sf::Color::White, 99, 280 );
+    sf::Text city = createText(regular, "Enter a City:", 23, sf::Color::White, 120, 340 );
+
+    // output texts
+    sf::Text packbags = createText(regular, "Prepare for these conditions: ", 28, sf::Color::White, 99, 480);
+    sf::Text precipitation = createText(regular, "Average Temp: ", 23, sf::Color::White, 120, 530);
+    sf::Text windspeed = createText(regular, "Precipitation: ", 23, sf::Color::White, 120, 565);
+    sf::Text temp = createText(regular, "Wind Speed: ", 23, sf::Color::White, 120, 595);
+
+    //create dropdowns
+    dropdown months(200, 225, 150, 30, regular,
+                    {"January", "February", "March", "April", "May", "June", "July",
+                     "August", "September", "October", "November", "December"}, 18);
+    dropdown weeks(470, 225, 100, 30, regular,
+                    {"1st", "2nd", "3rd", "4th", "5th"}, 18);
+
+    // make textbox
+    textbox citybox(250, 335, 400, 35, regular, 20);
+
+    while(welcomewindow.isOpen()){
+        sf::Event event;
+        while(welcomewindow.pollEvent(event)){
+            if(event.type == sf::Event::Closed){
+                welcomewindow.close();
+            }
+            months.onEvent(event);
+            weeks.onEvent(event);
+            citybox.onEvent(event);
+        }
+        //draw window and welcome texts
+        welcomewindow.clear(sf::Color(130, 220, 155));
+        welcomewindow.draw(welcometext); welcomewindow.draw(boldwelc);
+        welcomewindow.draw(exclamation); welcomewindow.draw(whenvisit);
+        welcomewindow.draw(month); welcomewindow.draw(week);
+        welcomewindow.draw(wherego); welcomewindow.draw(city);
+
+        //draw result texts
+        welcomewindow.draw(packbags);
+        welcomewindow.draw(precipitation);
+        welcomewindow.draw(windspeed);
+        welcomewindow.draw(temp);
+
+        //draw textbox
+        citybox.draw(welcomewindow);
+
+        //draw dropdowns
+        months.draw(welcomewindow);
+        weeks.draw(welcomewindow);
+
+        welcomewindow.display();
+
+
+    }
+
+    return 0;
+}
