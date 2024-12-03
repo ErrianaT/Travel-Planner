6d946cefae8869b25af8a993eed650488e293030
soficruz2004 <149535446+soficruz2004@users.noreply.github.com>
2024-12-03 14:02:29 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,25 +26,35 @@ int main() {
     sf::Text exclamation = createText(bolditalic, "!", 35, sf::Color::White, 635, 100);
     sf::Text whenvisit = createText(regular, "When are you planning your visit?", 28, sf::Color::White, 99, 170 );
     sf::Text month = createText(regular, "Month: ", 23, sf::Color::White, 120, 230 );
-    sf::Text week = createText(regular, "Week: ", 23, sf::Color::White, 395, 230 );
+    sf::Text Day = createText(regular, "Day: ", 23, sf::Color::White, 395, 230 );
     sf::Text wherego = createText(regular, "Where would you like to go?", 28, sf::Color::White, 99, 280 );
     sf::Text city = createText(regular, "Enter a City:", 23, sf::Color::White, 120, 340 );
 
     // output texts
-    sf::Text packbags = createText(regular, "Prepare for these conditions: ", 28, sf::Color::White, 99, 480);
-    sf::Text precipitation = createText(regular, "Average Temp: ", 23, sf::Color::White, 120, 530);
-    sf::Text windspeed = createText(regular, "Precipitation: ", 23, sf::Color::White, 120, 565);
-    sf::Text temp = createText(regular, "Wind Speed: ", 23, sf::Color::White, 120, 595);
+    sf::Text packbags = createText(regular, "Prepare for these conditions! ", 28, sf::Color::White, 99, 480);
+    sf::Text temp = createText(regular, "Average Temp: ", 23, sf::Color::White, 120, 530);
+    sf::Text precipitation = createText(regular, "Precipitation: ", 23, sf::Color::White, 120, 565);
+    sf::Text  windspeed = createText(regular, "Wind Speed: ", 23, sf::Color::White, 120, 595);
 
-    //create dropdowns
+    //create dropdown
     dropdown months(200, 225, 150, 30, regular,
                     {"January", "February", "March", "April", "May", "June", "July",
                      "August", "September", "October", "November", "December"}, 18);
-    dropdown weeks(470, 225, 100, 30, regular,
-                    {"1st", "2nd", "3rd", "4th", "5th"}, 18);
+    //dropdown weeks(470, 225, 100, 30, regular,
+                  //  {"1st", "2nd", "3rd", "4th", "5th"}, 18);
 
-    // make textbox
+    // make textboxs
+    textbox day(470, 225, 100, 30, regular, 20);
     textbox citybox(250, 335, 400, 35, regular, 20);
+
+    // make button
+    clickbutton enterbutton(300, 400, 200, 50, regular, "Search", 24);
+
+    // make output values text
+    sf::Text tempresult = createText(regular, " ", 23, sf::Color::White, 250, 530);
+    sf::Text precresult = createText(regular, " ", 23, sf::Color::White, 250, 565);
+    sf::Text wsresult = createText(regular, " ", 23, sf::Color::White, 250, 595);
+
 
     while(welcomewindow.isOpen()){
         sf::Event event;
@@ -52,15 +62,26 @@ int main() {
             if(event.type == sf::Event::Closed){
                 welcomewindow.close();
             }
+            enterbutton.onEvent(event, welcomewindow);
             months.onEvent(event);
-            weeks.onEvent(event);
+            day.onEvent(event);
             citybox.onEvent(event);
         }
+        if(enterbutton.isclicked()){
+            // output results on button click
+            tempresult.setString(" degrees");
+            precresult.setString(" inches");
+            wsresult.setString(" mph");
+            //reset
+            enterbutton.resetbutton();
+
+        }
+
         //draw window and welcome texts
         welcomewindow.clear(sf::Color(130, 220, 155));
         welcomewindow.draw(welcometext); welcomewindow.draw(boldwelc);
         welcomewindow.draw(exclamation); welcomewindow.draw(whenvisit);
-        welcomewindow.draw(month); welcomewindow.draw(week);
+        welcomewindow.draw(month); welcomewindow.draw(Day);
         welcomewindow.draw(wherego); welcomewindow.draw(city);
 
         //draw result texts
@@ -71,14 +92,21 @@ int main() {
 
         //draw textbox
         citybox.draw(welcomewindow);
+        day.draw(welcomewindow);
 
-        //draw dropdowns
+        //draw dropdown
         months.draw(welcomewindow);
-        weeks.draw(welcomewindow);
+
+        //drawbutton
+        enterbutton.draw(welcomewindow);
+
+        if(enterbutton.isclicked()){
+            welcomewindow.draw(tempresult);
+            welcomewindow.draw(precresult);
+            welcomewindow.draw(wsresult);
+        }
 
         welcomewindow.display();
-
-
     }
 
     return 0;
